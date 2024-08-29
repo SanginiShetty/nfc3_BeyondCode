2929385d589633374712abdcd1bfd050091ab85c
Sridhar1030 <77533416+Sridhar1030@users.noreply.github.com>
2024-08-29 13:36:01 +0530

    .

--- backend/app.js
+++ backend/app.js
@@ -13,7 +13,8 @@ app.use(express.json());
 app.use(cookieParser());
 
 import { userRouter } from "./routers/user.routes.js";
-import { projectRouter } from "./routers/project.routes.js";
+import { projectRouter } from "./routers/project.routes.js";\
+
 
 app.use("/api/v1/auth", userRouter);
 app.use("/api/v1/project",projectRouter)
