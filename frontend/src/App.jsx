99d826282febad75e4a92b006d7fe19335d21876
SHUBHAM SINGH CDFX19 <146632720+Shubham-byte-ai@users.noreply.github.com>
2024-08-29 13:57:27 +0530

    Home

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import Login from './components/Login'; // Make sure the path is correct
 import SignUp from './components/SignUp';
+import Home from './components/Home';
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Routes>
         <Route path="/login" element={<Login />} />
         <Route path="/signup" element={<SignUp />} />
-        
+        <Route path="/" element={<Home />} />
+
         {/* Add more routes here */}
         {/* <Route path="/" element={<Home />} /> */}
         {/* <Route path="/dashboard" element={<Dashboard />} /> */}
